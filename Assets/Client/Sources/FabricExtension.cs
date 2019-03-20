d9ba7bd4db41e573abae00b739c51408c5908a64
foryourselfand <foryourselfand@gmail.com>
2019-03-20 15:56:41 +0300

    placing

---
+++ Assets/Client/Sources/FabricExtension.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public static class FabricExtension
+{
+	public static void CreateShadowAndGlass(this GameContext context, int x)
+	{
+		var shadowGameObject = InstantiateFromAsset("shadow");
+		var shadowEntity = context.CreateEntity();
+		shadowEntity.AddGameObject(shadowGameObject);
+		shadowEntity.AddPosition(new Vector2(x, -30));
+
+		var glassEntity = context.CreateEntity();
+		glassEntity.AddGameObject(InstantiateFromAsset("glass"));
+		glassEntity.AddPosition(new Vector2(0, 9));
+		glassEntity.AddParentTransform(shadowGameObject.transform);
+	}
+
+	private static GameObject InstantiateFromAsset(string path)
+	{
+		var prefab = Resources.Load<GameObject>(path);
+		var go = Object.Instantiate(prefab);
+		return go;
+	}
+}
--- Assets/Client/Sources/Gang/CreateGangSystem.cs
+++ Assets/Client/Sources/Gang/CreateGangSystem.cs
@@ -7,31 +7,17 @@ using Object = UnityEngine.Object;
 public class CreateGangSystem : IInitializeSystem
 {
 	private readonly Contexts _contexts;
-	private readonly Transform _viewParent;
 
 	public CreateGangSystem(Contexts contexts)
 	{
 		_contexts = contexts;
-		_viewParent = new GameObject("Views").transform;
 	}
 
 	public void Initialize()
 	{
-		var shadowGameObject = InstantiateFromAsset("shadow");
-		var shadowEntity = _contexts.game.CreateEntity();
-		shadowEntity.AddGameObject(shadowGameObject);
-		shadowEntity.AddPosition(new Vector2(-20, -20));
-
-		var glassEntity = _contexts.game.CreateEntity();
-		glassEntity.AddGameObject(InstantiateFromAsset("glass"));
-		glassEntity.AddPosition(new Vector2(0, 9));
-		glassEntity.AddParentTransform(shadowGameObject.transform);
-	}
-
-	private GameObject InstantiateFromAsset(string path)
-	{
-		var prefab = Resources.Load<GameObject>(path);
-		var go = Object.Instantiate(prefab, _viewParent);
-		return go;
+		for (var i = -45; i <= 45; i += 30)
+		{
+			_contexts.game.CreateShadowAndGlass(i);
+		}
 	}
 }
